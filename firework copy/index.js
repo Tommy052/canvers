4c09dec35ddded57b20c5c77d52744d0bcd8023b
tommy <eheh34w@naver.com>
2023-05-22 14:43:11 +0900

    보일러플레이트
    보일러플레이트

---
+++ firework copy/index.js
@@ -0,0 +1,46 @@
+import CanvasOption from "./js/CanvasOption.js"
+
+
+
+class Canvas extends CanvasOption {
+  constructor() {
+    super()
+  }
+  init() {
+    this.canvasWidth = innerWidth
+    this.canvasHeight = innerHeight
+    this.canvas.width = this.canvasWidth * this.dpr
+    this.canvas.height = this.canvasHeight * this.dpr
+    this.ctx.scale(this.dpr, this.dpr)
+  
+    this.canvas.style.width = this.canvasWidth + 'px'
+    this.canvas.style.height = this.canvasHeight + 'px'
+  }
+  reneder() {
+    let now, delta 
+    let then = Date.now()
+
+    const frame = () => {
+      requestAnimationFrame(frame)
+      now = Date.now()
+      delta = now - then 
+      if (delta < this.interval) return
+    
+      this.ctx.fillRect(100, 100, 200, 200)
+    
+      then = now - (delta % interval)
+    }
+    requestAnimationFrame(frame)
+  }
+}
+
+const canvas = new Canvas()
+
+window.addEventListener('load', () => {
+  canvas.init()
+  canvas.reneder()
+})
+
+window.addEventListener('resize',()=> {
+  canvas.init()
+})
